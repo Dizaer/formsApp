549240b322d1f5e6ebb66f16102c04f8bff63bed
IE Dev 2 <ie.dev.2@hotmail.com>
2024-10-29 12:59:04 -0700

    257. Elaborar el validador de correo

--- src/app/shared/validators/email.validator.service.ts
+++ src/app/shared/validators/email.validator.service.ts
@@ -8,13 +8,38 @@ export class EmailValidatorService implements AsyncValidator {
     validate(control: AbstractControl): Observable<ValidationErrors | null> {
         
         const email = control.value;
-        console.log({email});
+        
+        const httpCallObservable = new Observable<ValidationErrors|null>( (subscriber) =>{
+        
+            console.log({email});
 
-        return of({
-            emailTaken: true
+            if ( email === 'fernando@google.com') {
+                subscriber.next({ emailTaken: true });
+                subscriber.complete();
+                // return;
+            }
+
+            subscriber.next( null );
+            subscriber.complete();
+
         }).pipe(
-            delay( 2000 )
+            delay(2000)
         );
+
+        return httpCallObservable;
+
     }
+
+    // validate(control: AbstractControl): Observable<ValidationErrors | null> {
+        
+    //     const email = control.value;
+    //     console.log({email});
+
+    //     return of({
+    //         emailTaken: true
+    //     }).pipe(
+    //         delay( 2000 )
+    //     );
+    // }
     
 }
