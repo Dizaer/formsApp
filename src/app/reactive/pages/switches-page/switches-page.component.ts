d26121d05be0328363eff5e177675254f45a4311
IE Dev 2 <ie.dev.2@hotmail.com>
2024-10-29 08:53:02 -0700

    250. Continuación de proyecto y tarea

--- src/app/reactive/pages/switches-page/switches-page.component.ts
+++ src/app/reactive/pages/switches-page/switches-page.component.ts
@@ -1,9 +1,24 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
-  templateUrl: './switches-page.component.html',
-  styles: ``
+  templateUrl: './switches-page.component.html'
 })
 export class SwitchesPageComponent {
+
+  public myForm: FormGroup = this.fb.group({
+    gender: ['M', Validators.required ],
+    wantNotifications: [ true, Validators.required ],
+    termsAndConditions: [ false, Validators.requiredTrue ]
+  })
+
+  constructor( private fb: FormBuilder ) {}
+
+  onSave():void {
+    if ( this.myForm.invalid ){
+      this.myForm.markAllAsTouched();
+      return;
+    }
+  }
 
 }
